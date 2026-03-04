95d4e2a033dda8d1ae6d5eeb200441a8009abc78
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-03-04 20:43:04 +0000

    first excercise completed

---
+++ modulo-1/ejercicio1.sql
@@ -0,0 +1,13 @@
+-- Creamos la tabla con la estructura que pide el Dr. Chuck
+CREATE TABLE track_raw (
+    title TEXT, 
+    artist TEXT, 
+    album TEXT,
+    count INTEGER, 
+    rating INTEGER, 
+    len INTEGER
+);
+
+\copy track_raw(title,artist,album,count,rating,len) FROM 'library.csv' WITH DELIMITER ',' CSV;
+
+SELECT title, album FROM track_raw ORDER BY title LIMIT 3
