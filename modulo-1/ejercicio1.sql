cddcf03c522255783368b39ab12096c57e6e5d18
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-03-04 20:53:53 +0000

    file .gitignore created

--- modulo-1/ejercicio1.sql
+++ modulo-1/ejercicio1.sql
@@ -8,6 +8,10 @@ CREATE TABLE track_raw (
     len INTEGER
 );
 
+-- 2. Cargar los datos (La magia del CSV)
+-- OJO: Asegúrate de que el nombre del archivo sea exacto
 \copy track_raw(title,artist,album,count,rating,len) FROM 'library.csv' WITH DELIMITER ',' CSV;
 
+
+-- 3. La prueba final (Lo que el profesor va a mirar)
 SELECT title, album FROM track_raw ORDER BY title LIMIT 3
